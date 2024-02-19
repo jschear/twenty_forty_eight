da4e976f44d9d7365466de1502850ae544c058e2
Jonathan Schear <jonathanschear@gmail.com>
2024-02-19 18:55:59 -0500

    Add vercel bits

--- ci/build.sh
+++ ci/build.sh
@@ -8,9 +8,8 @@ eval $(opam env)
 dune build bin/main.bc.js
 
 # Put the static files where vercel would like them to be
-local VERCEL_OUTPUT=.vercel/output/static/
+VERCEL_OUTPUT=.vercel/output/static/
 mkdir -p $VERCEL_OUTPUT
 cp public/index.html $VERCEL_OUTPUT
 cp _build/default/bin/main.bc.js $VERCEL_OUTPUT
 
-vercel deploy --prebuilt --prod
