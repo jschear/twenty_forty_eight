2a2df665063982c176c86741d2ca758bcc04924f
Jonathan Schear <jonathanschear@gmail.com>
2024-02-19 21:07:23 -0500

    Fix more vercel bits

--- ci/build.sh
+++ ci/build.sh
@@ -8,8 +8,13 @@ eval $(opam env)
 dune build bin/main.bc.js
 
 # Put the static files where vercel would like them to be
-VERCEL_OUTPUT=.vercel/output/static/
-mkdir -p $VERCEL_OUTPUT
-cp public/index.html $VERCEL_OUTPUT
-cp _build/default/bin/main.bc.js $VERCEL_OUTPUT
+VERCEL_OUTPUT=.vercel/output
+mkdir -p "$VERCEL_OUTPUT/static/"
+cp public/index.html "$VERCEL_OUTPUT/static/"
+cp _build/default/bin/main.bc.js "$VERCEL_OUTPUT/static/"
 
+cat <<EOF > "$VERCEL_OUTPUT/"
+{
+  "version": 3
+}
+EOF
